635b61fff7fee7532885e81abe83f0bdca0bee3a
Iskandar Arba <iskandararba@gmail.com>
2021-05-19 08:52:34 +0800

    add flash message

--- hello.py
+++ hello.py
@@ -1,7 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secret key"
+
+
+# Create a Form class
+class NamerForm(FlaskForm):
+    name = StringField('What Your Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    # BooleanField
+	# DateField
+	# DateTimeField
+	# DecimalField
+	# FileField
+	# HiddenField
+	# MultipleField
+	# FieldList
+	# FloatField
+	# FormField
+	# IntegerField
+	# PasswordField
+	# RadioField
+	# SelectField
+	# SelectMultipleField
+	# SubmitField
+	# StringField
+	# TextAreaField
+
+	## Validators
+	# DataRequired
+	# Email
+	# EqualTo
+	# InputRequired
+	# IPAddress
+	# Length
+	# MacAddress
+	# NumberRange
+	# Optional
+	# Regexp
+	# URL
+	# UUID
+	# AnyOf
+	# NoneOf
+
 
 # Create a route decorator
 @app.route('/')
@@ -38,3 +85,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+
+# Create Name Page
+@app.route('/name', methods=['POST', 'GET'] )
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash('From Submitted Succesfully!!', 'success')
+
+    return render_template('name.html', name=name, form=form)
